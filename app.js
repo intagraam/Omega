192405b48c1f2f123c9670cf7aa95f9e79f34fd3
intagraam <69187561+intagraam@users.noreply.github.com>
2020-08-04 12:27:15 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,15 +3,15 @@
 
 ///Edit Under This
 
-var firebaseConfig = {
-  apiKey: "api-key",
-  authDomain: "project-id.firebaseapp.com",
-  databaseURL: "https://project-id.firebaseio.com",
-  projectId: "project-id",
-  storageBucket: "project-id.appspot.com",
-  messagingSenderId: "sender-id",
-  appId: "app-id",
-  measurementId: "G-measurement-id",
+const firebaseConfig = {
+  apiKey: "AIzaSyC2K7nEm4krLk_UVnACBnWEh5G4qGqH2Ag",
+  authDomain: "inatagram-28975.firebaseapp.com",
+  databaseURL: "https://inatagram-28975.firebaseio.com",
+  projectId: "inatagram-28975",
+  storageBucket: "inatagram-28975.appspot.com",
+  messagingSenderId: "521411264307",
+  appId: "1:521411264307:web:8a37e201dbde7e6ad70846",
+  measurementId: "G-CNQTNZ9T42"
 };
 
      
